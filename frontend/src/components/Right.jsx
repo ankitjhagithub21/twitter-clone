4b862d02fb8ad82a0bce5fbef4d2ba720d5d49d1
ankitjhagithub21 <ajha45075@gmail.com>
2024-07-09 19:38:19 +0530

    design search bar

--- frontend/src/components/Right.jsx
+++ frontend/src/components/Right.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import WhoToFollow from './WhoToFollow'
 import Premium from './Premium'
+import SearchBar from './SearchBar'
 
 const Right = () => {
   return (
     <div className='lg:w-[30%] w-full md:block hidden p-3 overflow-auto lg:flex flex-col gap-5'>
+      <SearchBar/>
       <Premium/>
       <WhoToFollow/>
     </div>
---
+++ frontend/src/components/SearchBar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { CiSearch } from 'react-icons/ci'
+
+const SearchBar = () => {
+  return (
+    <div className='flex items-center gap-5 bg-[#202327] text-white px-4 text-sm py-2 rounded-full'>
+        <CiSearch size={20}/>
+      <input type="text" placeholder='Search' className='bg-transparent' />
+    </div>
+  )
+}
+
+export default SearchBar
