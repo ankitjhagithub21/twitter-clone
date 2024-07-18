879e8d94d478c6febf64d9c62b5a28d0c9612812
ankitjhagithub21 <ajha45075@gmail.com>
2024-07-18 10:35:41 +0530

    changes tweet

--- frontend/src/components/Tweet.jsx
+++ frontend/src/components/Tweet.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import toast from 'react-hot-toast';
 import { CiHeart, CiRepeat, CiBookmark, CiTrash } from "react-icons/ci";
 import { FaRegComment } from "react-icons/fa6";
@@ -7,13 +7,14 @@ import { useDispatch, useSelector } from 'react-redux';
 import { likeUnlikeTweet, removeTweet } from '../redux/slices/tweetSlice';
 import { useNavigate } from "react-router-dom";
 import formatDate from '../helpers/formateDate';
+import Loader from './Loader';
 
 const Tweet = ({ tweet }) => {
     const dispatch = useDispatch();
     const { currUser } = useSelector(state => state.user);
     const navigate = useNavigate();
     const formattedDate = formatDate(tweet.createdAt);
-
+    const [loading, setLoading] = useState(false)
     const handleDelete = async () => {
         try {
             const res = await fetch(`${import.meta.env.VITE_SERVER_URL}/api/tweet/${tweet._id}`, {
@@ -38,6 +39,7 @@ const Tweet = ({ tweet }) => {
     };
 
     const handleLikeUnlike = async () => {
+        setLoading(true)
         try {
             const res = await fetch(`${import.meta.env.VITE_SERVER_URL}/api/tweet/like/${tweet._id}`, {
                 method: 'PUT',
@@ -47,13 +49,13 @@ const Tweet = ({ tweet }) => {
 
             if (data.success) {
                 dispatch(likeUnlikeTweet({ tweetId: tweet._id, userId: currUser._id }));
-                toast.success(data.message);
-            } else {
-                toast.error(data.message);
+
             }
         } catch (error) {
-            toast.error("Something went wrong.");
+
             console.error(error);
+        } finally {
+            setLoading(false)
         }
     };
 
@@ -75,20 +77,22 @@ const Tweet = ({ tweet }) => {
                         <img src={tweet.image.url} alt="thumbnail" className='rounded-lg object-contain h-56' />
                     </div>
                 )}
-                <div className='flex items-center justify-between text-lg mt-2'>
+                <div className='flex items-center justify-between text-lg mt-3'>
                     <div>
-                        <FaRegComment />
+                        <FaRegComment size={22} />
                     </div>
                     <button className='flex items-center' onClick={handleLikeUnlike}>
-                        {tweet.likes.length}
-                        <CiHeart />
+                        {
+                            loading ? <Loader /> : tweet.likes.length
+                        }
+                        <CiHeart size={25} />
                     </button>
                     <div>
-                        <CiRepeat />
+                        <CiRepeat size={25} />
                     </div>
                     <div className='cursor-pointer'>
                         {currUser._id === tweet.author._id ? (
-                            <CiTrash onClick={handleDelete} />
+                            <CiTrash onClick={handleDelete} size={25} />
                         ) : (
                             <CiBookmark />
                         )}
