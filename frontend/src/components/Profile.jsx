016ea2760691cec8d790db5422d0f6578837ada9
ankitjhagithub21 <ajha45075@gmail.com>
2024-07-18 10:19:55 +0530

    added edit profile button

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -4,18 +4,18 @@ import useGetProfileUser from '../hooks/useGetProfileUser';
 import Loader from './Loader';
 import { IoIosArrowRoundBack } from "react-icons/io";
 import UserNotFound from './UserNotFound';
-import { CiLocationOn } from "react-icons/ci";
 import { SlCalender } from "react-icons/sl";
 import formatDate from '../helpers/formateDate';
+import { useSelector } from 'react-redux';
 
 const Profile = () => {
     const { username } = useParams();
     const { user, loading } = useGetProfileUser(username);
-
+    const {currUser} = useSelector(state=>state.user)
 
     if (loading) {
         return <div className='p-5'>
-            <Loader />
+             <Loader />
         </div>;
     }
 
@@ -27,12 +27,18 @@ const Profile = () => {
         <div className='h-full overflow-auto'>
             <div className='p-2'>
                 <Link to={"/"} className='flex gap-5 items-center '>
-                    <IoIosArrowRoundBack size={25} />
+                  <button className='p-2 hover:bg-gray-800 rounded-full'>
+                  <IoIosArrowRoundBack size={25} />
+                  </button>
                     <span className='text-xl font-semibold'>Profile</span>
                 </Link>
             </div>
-            <div className='bg-[#333639] h-48'></div>
-            <div className='-mt-16 ml-5'>
+            <div className='bg-[#333639] h-48 relative'>
+           {
+            currUser._id === user._id &&      <button className='absolute bottom-2 right-3 z-10 border rounded-full px-4 py-2 hover:bg-gray-800'>Edit Profile</button>
+           }
+            </div>
+            <div className='-mt-16 ml-5 relative'>
                 <div className='rounded-full w-32 h-32 bg-[#333639] border '>
                     <img src={user.profileImg} alt="" />
                 </div>
